198e434ccfcc1486fe47868b338b182f98a712ea
arthurbnu <arthur.brody@bnu.fr>
2024-03-27 14:14:07 +0100

    Ajouter le lecteur de code-barres Vue et les dépendances associées

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,55 @@
 <template>
   <main class="min-h-screen">
-    <div class="space-y-24">
+    <div class="space-y-5">
 
       <h1 class="text-primary">AIDONS</h1>
       <h2 class="tracking-wider opacity-80 italic">Aide au signalement par l'IA</h2>
 
 
       <!--  input opening the camera on mobile -->
-      <div class="flex ">
-       <input type="file" accept="image/*" capture="camera" @click="openCamera" class="bg-primary/50 text-white p-2 rounded-lg cursor-pointer" />
+      <!-- <div class=" ">
+        <input type="file" accept="image/*" capture="camera" @click="handleInput"
+          class="bg-primary/50 text-white p-2 rounded-lg cursor-pointer" />
+      </div> -->
+
+      <div v-if="error">
+        <p class="text-red-500">{{ error }}</p>
       </div>
 
+      <div class="flex gap-3 text-lg">
+        <p class="">Code barre décodé</p>
+        <p class="text-primary">{{ decodedText }}</p>
+      </div>
 
+      <div>
+        <p>Placer un code barre devant la caméra Ou choisir un fichier dans la gallerie</p>
+      </div>
+
+      <ImageBarcodeReader @decode="onDecode" @error="onError" class="text-primary-700"></ImageBarcodeReader>
+      <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
     </div>
   </main>
 </template>
 
 <script setup>
+import { StreamBarcodeReader, ImageBarcodeReader } from "vue-barcode-reader";
+const decodedText = ref("______");
+const error = ref('');
 
-// const 
+const onDecode = (decodedValue) => {
+  decodedText.value = decodedValue
+}
 
-const openCamera = () => {
-  console.log('Opening camera')
-  // Open the camera
-  // This is a placeholder for the actual code that will open the camera
+const onError = (error) => {
+  error.value = error
+}
+
+const onLoaded = () => {
+  console.log('Barcode reader loaded')
+}
+
+const handleInput = () => {
+  console.log('handleInput')
 }
 
 </script>
