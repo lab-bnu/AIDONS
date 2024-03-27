1ec6eef6e64c83a083e8de2acfae4287b75fe6d2
arthurbnu <arthur.brody@bnu.fr>
2024-03-27 12:01:22 +0100

    Mise à jour des dépendances npm et modifications de l'interface utilisateur

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
   <main class="min-h-screen">
     <div class="space-y-24">
-      <HomeIntro />
-      <HomeSocialLinks />
-      <HomeFeaturedProjects />
-      <HomeFeaturedArticles />
-      <HomeNewsletter />
+
+      <h1 class="text-primary">AIDONS</h1>
+      <h2 class="tracking-wider opacity-80 italic">Aide au signalement par l'IA</h2>
+
+
+      <!--  input opening the camera on mobile -->
+      <div class="flex ">
+       <input type="file" accept="image/*" capture="camera" @click="openCamera" class="bg-primary/50 text-white p-2 rounded-lg cursor-pointer" />
+      </div>
+
+
     </div>
   </main>
 </template>
+
+<script setup>
+
+// const 
+
+const openCamera = () => {
+  console.log('Opening camera')
+  // Open the camera
+  // This is a placeholder for the actual code that will open the camera
+}
+
+</script>
