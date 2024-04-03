edc8e500f00a7caf5ac72fb00525887062586098
arthurbnu <arthur.brody@bnu.fr>
2024-04-03 14:12:21 +0200

    Ajouter des exemples de codes barres et des liens pour la recherche

--- components/App/BarCode.client.vue
+++ components/App/BarCode.client.vue
@@ -14,9 +14,14 @@
       <p class="text-red-500">{{ error }}</p>
     </div>
 
+    <p class="text-sm text-gray-500">
+      Essayer avec un exemple : 
+      <span v-for="code in barCodeExamples" :key="code" @click="decodedText = code" 
+      class="cursor-pointer text-primary-700 hover:text-primary-500 m-2">{{ code }}</span>
+    </p>
     <div class="flex gap-3 text-lg">
       <p class="">Code barre décodé</p>
-      <p class="text-primary">{{ decodedText }}</p>
+      <p class="text-primary">{{ decodedText || 'Aucun code barre trouvé'}}</p>
     </div>
 
     <p>Placer un code barre devant la caméra Ou choisir un fichier dans la gallerie</p>
@@ -26,14 +31,9 @@
     <div>
       <span class="text-primary">Liens</span>
       <ul class="pl-3 [&>*:hover]:text-primary">
-        <li><a :href='"https://isbndb.com/book/" + decodedText'>isbndb</a></li>
-        <li><a :href='"https://www.sudoc.fr/services/isbn2ppn/" + decodedText'>sudoc - ppn</a></li>
-        <li><a :href="'https://www.google.com/search?q=' + decodedText">google</a></li>
-        <li><a :href='"https://www.amazon.fr/s?k=" + decodedText'>amazon</a></li>
-        <li><a :href='"https://www.fnac.com/SearchResult/ResultList.aspx?SCat=0%211&Search=" + decodedText'>fnac</a></li>
-        <li><a :href='"https://www.livre-rare-book.com/search/current.seam?isbn=" + decodedText'>livre rare book</a></li>
-        <li><a :href='"https://www.abebooks.fr/servlet/SearchResults?isbn=" + decodedText'>abebooks</a></li>
-        <li><a :href='"https://www.abebooks.fr/servlet/SearchResults?isbn=" + decodedText'>abebooks</a></li>
+        <li v-for="(value, key) in links" :key="key">
+          <a :href="value + decodedText" target="_blank">{{ key }}</a>
+        </li>
       </ul>
     </div>
 
@@ -55,11 +55,8 @@
 
 <script setup>
 import { StreamBarcodeReader, ImageBarcodeReader } from "vue-barcode-reader";
-// const decodedText = ref("9780545010221");
-const decodedText = ref("");
-// const decodedText = ref("9783161484100");
 const error = ref('');
-
+const decodedText = ref("");
 // openLibrary API
 const openLibUrl = 'https://openlibrary.org/api/books?bibkeys=ISBN:';
 const url = computed(() => openLibUrl + decodedText.value + '&format=json&jscmd=data');
@@ -68,6 +65,21 @@ const { data: openLibData, error: openLiberror } = await useFetch(url);
 // API for cover image
 const coverImageUrl = 'https://covers.openlibrary.org/b/isbn/';
 const coverImage = coverImageUrl + decodedText.value + '-M.jpg';
+
+const barCodeExamples = [
+  '9780545010221',
+  '9783161484100',
+]
+
+const links = {
+  isbndb: 'https://isbndb.com/book/',
+  sudoc: 'https://www.sudoc.fr/services/isbn2ppn/',
+  google: 'https://www.google.com/search?q=',
+  amazon: 'https://www.amazon.fr/s?k=',
+  fnac: 'https://www.fnac.com/SearchResult/ResultList.aspx?SCat=0%211&Search=',
+  livreRareBook: 'https://www.livre-rare-book.com/search/current.seam?isbn=',
+  abebooks: 'https://www.abebooks.fr/servlet/SearchResults?isbn='
+}
 
 const onDecode = (decodedValue) => {
   decodedText.value = decodedValue
@@ -80,6 +92,8 @@ const onError = (error) => {
 const onLoaded = () => {
   console.log('Barcode reader loaded')
 }
+
+
 
 const handleInput = () => {
   console.log('handleInput')
