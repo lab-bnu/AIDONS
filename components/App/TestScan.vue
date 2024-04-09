39c67774849691ab21a5bf09d80e9f0455c859d6
arthurbnu <arthur.brody@bnu.fr>
2024-04-09 16:45:50 +0200

    Mise à jour du composant TestScan.vue et de la page test-2.vue

--- components/App/TestScan.vue
+++ components/App/TestScan.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-        <qrcode-stream @detect="onDetect" :formats="ean_13"></qrcode-stream>
+        <!-- <qrcode-stream @detect="onDetect" :formats="ean_13"></qrcode-stream> -->
+        <qrcode-stream @detect="onDetect" ></qrcode-stream>
+        <p>Résultat : {{ result }}</p>
     </div>
-    <p>Résultat : {{ result }}</p>
 </template>
 
 
 <script setup>
 import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+const result = ref('')
 const onDetect = (decodedText) => {
     console.log(decodedText)
     result.value = decodedText
