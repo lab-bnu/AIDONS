4bdbd59f2d99c788929fa9c4f80d465a6f620f83
elisa.michelet <elisa.michelet@bnu.fr>
2024-04-09 14:53:35 +0200

    test qrcode reader

---
+++ components/App/TestScan.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <qrcode-stream @detect="onDetect"></qrcode-stream>
+    </div>
+    <p>Résultat : {{ result }}</p>
+</template>
+
+
+<script setup>
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+const onDetect = (decodedText) => {
+    console.log(decodedText)
+    result.value = decodedText
+}
+</script>
---
+++ pages/test-elisa.vue
@@ -0,0 +1,3 @@
+<template>
+    <AppTestScan></AppTestScan>
+</template>
