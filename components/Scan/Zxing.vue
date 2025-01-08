bff59c6a9e42a0ac2e79645391ee6281fb5dbe0d
arthurbnu <arthur.brody@bnu.fr>
2025-01-08 17:29:45 +0100

    Refactor Zxing.vue: - début gestion des formats de code-barres et suppression des fichiers z.vue et zxing.vue

--- components/Scan/Zxing.vue
+++ components/Scan/Zxing.vue
@@ -1,56 +1,52 @@
 <template>
 
     <video ref="video" width="600" height="400" style="border: 1px solid black;"></video>
+    <button id="salut" @click="codeReader.reset()"></button>
 
 </template>
 
 
 <script setup>
 
-
-import { BrowserBarcodeReader } from '@zxing/library';
+import { BrowserBarcodeReader, BarcodeFormat } from '@zxing/library';
 
 const decoded = defineModel({
     type: String,
     default: ''
 })
 
+// mettre en place les hints
+const hints = new Map();
+hints.set(BarcodeFormat.EAN_13, {})
+
 const video = ref(null);
-const codeReader = new BrowserBarcodeReader();
+const codeReader = new BrowserBarcodeReader()
 
 const startCamera = async () => {
-    try {
-        const constraints = {
-            video: {
-                facingMode: { ideal: "environment" } // Utiliser la caméra arrière si disponible
-            }
-        };
-
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        video.value.srcObject = stream;
-
-        codeReader.decodeFromVideoElement(video.value)
-            .then(result => {decoded.value = result; codeReader.reset();})
-            .catch(err => console.error(err));
-    } catch (error) {
-        if (error.name === 'OverconstrainedError') {
-            console.warn('Camera arrière non disponible, utilisation de la caméra par défaut.');
-            // Essayer avec des contraintes plus flexibles
-            const fallbackConstraints = { video: true };
-            const fallbackStream = await navigator.mediaDevices.getUserMedia(fallbackConstraints);
-            video.value.srcObject = fallbackStream;
-
-            codeReader.decodeFromVideoElement(video.value)
-            .then(result => {decoded.value = result; codeReader.reset();})
-                .catch(err => console.error(err));
-        } else {
-            console.error('Error accessing the camera:', error);
+    const constraints = {
+        video: {
+            facingMode: { ideal: "environment" } // Utiliser la caméra arrière si disponible
         }
-    }
+    };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    video.value.srcObject = stream;
+
+    codeReader.decodeFromVideoElement(video.value)
+        .then(result => { decoded.value = result.text; })
+        .catch(err => console.error(err));
 };
+
+
 
 onMounted(() => {
     startCamera();
+
+    setTimeout(() => {
+
+
+        // test
+    }, 2000);
 });
 
 </script>
