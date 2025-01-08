5b130e1d43d4883b366d2548c80c9f2a872070ed
arthurbnu <arthur.brody@bnu.fr>
2025-01-08 15:47:33 +0100

    test caméra arrière

--- pages/zxing.vue
+++ pages/zxing.vue
@@ -11,20 +11,34 @@
 
 onMounted(async () => {
 
-const script = document.createElement('script');
-script.src = 'https://unpkg.com/@zxing/library@latest';
-document.head.appendChild(script);
+    const script = document.createElement('script');
+    script.src = 'https://unpkg.com/@zxing/library@latest';
+    document.head.appendChild(script);
 
-await new Promise(r => setTimeout(r, 2000));
+    await new Promise(r => setTimeout(r, 2000));
 
-const codeReader = new ZXing.BrowserBarcodeReader();
-codeReader.getVideoInputDevices()
-    .then(videoInputDevices => {
-        codeReader.decodeFromInputVideoDevice(videoInputDevices[0] ? videoInputDevices[0].deviceId : undefined, 'video')
-            .then(result => alert(result))
-            .catch(err => console.error(err));
-    })
-    .catch(err => console.error(err));
+    async function startCamera() {
+        try {
+            const constraints = {
+                video: {
+                    facingMode: { exact: "environment" } // Forcer l'utilisation de la caméra arrière
+                }
+            };
 
-})
+            const stream = await navigator.mediaDevices.getUserMedia(constraints);
+            const video = document.getElementById('video');
+            video.srcObject = stream;
+
+            const codeReader = new ZXing.BrowserBarcodeReader();
+            codeReader.decodeFromVideoElement(video)
+                .then(result => console.log(result))
+                .catch(err => console.error(err));
+        } catch (error) {
+            console.error('Error accessing the camera:', error);
+        }
+    }
+
+    startCamera();
+
+});
 </script>
